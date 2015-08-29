f4eb2aaa8cae1445aa0a1ee8867cb99c174566e3
Pawel Sierszen <pawelsierszen@gmail.com>
2015-08-29 15:02:10 +0200

    Draw lines and data points.

--- graphs.js
+++ graphs.js
@@ -6,6 +6,8 @@
 (function (document) {
     'use strict';
 
+    var SVG_NS = 'http://www.w3.org/2000/svg';
+
     /**
      * Helper for looping through collections.
      * @param {HTMLCollection} collection
@@ -23,6 +25,7 @@
     function Series(label) {
         this.label = label;
         this.data = [];
+        this.max = 0;
     }
 
     /**
@@ -30,6 +33,7 @@
      * @param {number} element
      */
     Series.prototype.add = function (element) {
+        this.max = Math.max(element, this.max);
         this.data.push(element);
     };
 
@@ -48,7 +52,7 @@
      * @return {object}
      */
     function getData(graph) {
-        var labels = [], series = [];
+        var labels = [], series = [], max = 0;
 
         // Read headers.
         forEach(graph.querySelectorAll('thead th'), function (th, colIndex) {
@@ -71,7 +75,10 @@
                 series[colIndex - 1].add(parseFloat(td.innerText));
             });
         });
-        return {labels: labels, series: series};
+        series.forEach(function (series) {
+            max = Math.max(max, series.max);
+        });
+        return {labels: labels, series: series, max: max};
     }
 
     /**
@@ -79,7 +86,69 @@
      * @param {HTMLTableElement} graph Graph element.
      */
     function transform(graph) {
-        var data = getData(graph);
+        var data = getData(graph),
+            container = document.createElement('div'),
+            svg = document.createElementNS(SVG_NS, 'svg'),
+            height = 100,
+            margin = 50,
+            width = 400;
+
+        // Set svg size to graph size plus some margin.
+        svg.setAttributeNS(null, 'height', height + margin);
+        svg.setAttributeNS(null, 'width', width + margin);
+
+        // Draw data points and lines.
+        data.series.forEach(function (series) {
+            var previousPoint, circles = [];
+            series.data.forEach(function (number, index) {
+                var circle = document.createElementNS(SVG_NS, 'circle'),
+                    line = document.createElementNS(SVG_NS, 'line'),
+                    point = {
+                        x: 60 * (index + 0.5),
+                        y: height - height * number / data.max
+                    };
+
+                // Initialize the previous point if not set.
+                previousPoint = previousPoint || {
+                    x: 0, y: height - (height - point.y) / 2
+                    };
+
+                line.setAttributeNS(null, 'x1', previousPoint.x + margin);
+                line.setAttributeNS(null, 'x2', point.x + margin);
+                line.setAttributeNS(null, 'y1', previousPoint.y + margin);
+                line.setAttributeNS(null, 'y2', point.y + margin);
+                line.setAttributeNS(null, 'stroke', '#15A0C8');
+                line.setAttributeNS(null, 'stroke-width', 4);
+
+                svg.appendChild(line);
+
+                circle.setAttributeNS(null, 'cx', point.x + margin);
+                circle.setAttributeNS(null, 'cy', point.y + margin);
+                circle.setAttributeNS(null, 'r',  5);
+                circle.setAttributeNS(null, 'fill', '#15A0C8');
+                circle.setAttributeNS(null, 'stroke', 'white');
+                circle.setAttributeNS(null, 'stroke-width', 3);
+                circles.push(circle);
+                previousPoint = point;
+            });
+            // Adding circles must be delayed, as they need
+            // to stay on top of the lines.
+            circles.forEach(function (circle) {
+                svg.appendChild(circle);
+            });
+        });
+
+        // Set the container class.
+        container.className = 'graph';
+
+        // Add the SVG into the container.
+        container.appendChild(svg);
+
+        // Insert the new container just before the old element.
+        graph.parentNode.insertBefore(container, graph);
+
+        // Hide the old element.
+        graph.style.display = 'none';
     }
 
     /**
